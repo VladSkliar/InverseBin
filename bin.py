967f1856dac2dada6f0e90a7536cc0e20fb6d595
VladSkliar <vlad.skliar@gmail.com>
2016-08-17 14:06:14 +0300

    Added check the input binary representation

--- bin.py
+++ bin.py
@@ -1,11 +1,11 @@
 import math
-# bin = [1, 0, 1, 1, 0, 1]
+# bin = [1, 0, 0, 1]
 
 
 def check_remain(x, bin_list):
     if (x % (-2)) == 0:
         bin_list.insert(0, 0)
-    elif (x % (-2)) != 0:
+    else:
         bin_list.insert(0, 1)
     return bin_list
 
@@ -14,7 +14,10 @@ def bin_inverse(bin_list):
     z = len(bin_list)
     x = 0
     for i in range(0, z):
-        x += bin_list[i] * ((-2)**(z - i - 1))
+        if bin_list[i] in (0, 1):
+            x += bin_list[i] * ((-2)**(z - i - 1))
+        else:
+            return "Error. You bin list is wrong. Check your input please."
     inverse_x = -x
     new_bin_list = []
     while inverse_x != 0:
@@ -26,4 +29,4 @@ def bin_inverse(bin_list):
         inverse_x = math.ceil(inverse_x / (-2))
     return new_bin_list
 
-# print bin_inverse(bin)
+# print (bin_inverse(bin))
